cb94c530ee5ecd81295fe516d5c55db5481016c5
rachdi <bigking3100@gmail.com>
2017-03-28 12:07:22 +0200

    Apres le lancement de laravel Création  d'une migration pour la table posts Lancement de la migration creation de UserTableSeeder.php creation de PostTableSeeder.php et lancement de la population avec php artisan db:seed Creation des modele Post et User

---
+++ blog/database/seeds/PostTableSeeder.php
@@ -0,0 +1,36 @@
+<?php 
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class PostTableSeeder extends Seeder
+{
+	private function randDate(){
+		return Carbon::createFromDate(null, rand(1,12),rand(1,28));
+	}
+	public function run()
+	{
+		DB::table('posts')->delete();
+		for ($i=0; $i <100 ; $i++) { 
+			$date=$this->randDate();
+			DB::table('posts')->insert([
+				'titre'=>'Titre'.$i,
+				'contenu'=>'Contenu'.$i.'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+				'user_id'=>rand(1,10),
+				'created_at'=>$date,
+
+				'updated_at'=>$date
+
+
+
+				]);
+		}
+
+
+
+	}
+
+}
+
+
+?>
